955a72d53300b830c7829536a7ca4d1ece11762a
Roman Maryatkin <rvmaryatkin@gmail.com>
2019-11-25 17:58:56 +0300

    first commit

---
+++ TicTacToe.py
@@ -0,0 +1,141 @@
+
+board = [' ' for x in range(10)]
+
+def insertLetter(letter, pos):
+    board[pos] = letter
+
+def spaceIsFree(pos):
+    return board[pos] == ' '
+
+def printBoard(board):
+    i = 1
+    while i < 10:
+        print('   |   |')
+        print(' ' + board[i] + ' | ' + board[i+1] + ' | ' + board[i+2])
+        print('   |   |')
+        i += 3
+        if i == 10:
+            break
+        print('-----------')
+
+def isWinner(bo, le):
+    return (bo[7] == le and bo[8] == le and bo[9] == le) \
+           or (bo[4] == le and bo[5] == le and bo[6] == le) \
+           or (bo[1] == le and bo[2] == le and bo[3] == le) \
+           or (bo[1] == le and bo[4] == le and bo[7] == le) \
+           or (bo[2] == le and bo[5] == le and bo[8] == le) \
+           or (bo[3] == le and bo[6] == le and bo[9] == le) \
+           or (bo[1] == le and bo[5] == le and bo[9] == le) \
+           or (bo[3] == le and bo[5] == le and bo[7] == le)
+
+
+def playerMove():
+    run = True
+    while run:
+        move = input('Please select a position to place an \'X\' (1-9): ')
+        try:
+            move = int(move)
+            if move > 0 and move < 10:
+                if spaceIsFree(move):
+                    run = False
+                    insertLetter('X', move)
+                else:
+                    print('Sorry, this space is already occupied!')
+            else:
+                print('Please enter a number within the range!')
+        except:
+            print('Please enter a number!')
+
+
+def compMove():
+    possibleMoves = [x for x, letter in enumerate(board) if letter == ' ' and x != 0]
+    move = 0
+
+    for let in ['O', 'X']:
+        for i in possibleMoves:
+            boardCopy = board[:]
+            boardCopy[i] = let
+            if isWinner(boardCopy, let):
+                move = i
+                return move
+
+    cornersOpen = []
+    for i in possibleMoves:
+        if i in [1, 3, 7, 9]:
+            cornersOpen.append(i)
+
+    if len(cornersOpen) > 0:
+        move = selectRandom(cornersOpen)
+        return move
+
+    if 5 in possibleMoves:
+        move = 5
+        return move
+
+    edgesOpen = []
+    for i in possibleMoves:
+        if i in [2, 4, 6, 8]:
+            edgesOpen.append(i)
+
+    if len(edgesOpen) > 0:
+        move = selectRandom(edgesOpen)
+
+    return move
+
+    if 5 in possibleMoves:
+        move = 5
+        return move
+
+    edgeOpen = []
+    for i in possibleMoves:
+        if i in [2, 4, 6, 8]:
+            edgeOpen.append(i)
+
+    if len(edgeOpen) > 0:
+        move = selectRandom(edgeOpen)
+        return move
+
+def selectRandom(li):
+    import random
+
+    ln = len(li)
+    r = random.randrange(0, ln)
+    return li[r]
+
+def isBoardFull(board):
+    if board.count(' ') > 1:
+        return False
+    else:
+        return True
+
+def main():
+    print('Welcome to TicTacToe!')
+    printBoard(board)
+
+    while not(isBoardFull(board)):
+        if not(isWinner(board, 'O')):
+            playerMove()
+            printBoard(board)
+        else:
+            print('You Suck')
+            break
+
+        if not(isWinner(board, 'X')):
+            move = compMove()
+            if move == 0:
+                print('Tie Game!')
+                break
+            else:
+                insertLetter('O', move)
+                print('Computer placed an \'O\' in position', move, ':')
+                printBoard(board)
+        else:
+            print('You Won')
+            break
+
+    if isBoardFull(board):
+        print('It is a tie')
+
+main()
+
+printBoard(board)
